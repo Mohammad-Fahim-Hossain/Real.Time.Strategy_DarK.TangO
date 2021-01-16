28ee667406014626dd89a2faa99d72d15d529e21
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2021-01-16 19:54:14 +0600

    RTSPlayer

---
+++ Real Time Strategy_ DarK TangO/Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField ]private List<Unit> MyUnits = new List<Unit>() ;
+
+    #region server
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    public void ServerHandleUnitSpawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != this.connectionToClient.connectionId) { return; }
+
+        MyUnits.Add(unit);
+    }
+
+    public void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != this.connectionToClient.connectionId) { return; }
+
+        MyUnits.Remove(unit);
+
+    }
+    #endregion
+
+
+    #region Client
+    public override void OnStartClient()
+    {
+        if (!isClientOnly) { return; }
+
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly) { return; }
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+    }
+
+    public void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        MyUnits.Add(unit);
+    }
+
+    public void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        if (!hasAuthority) { return; }
+
+        MyUnits.Remove(unit);
+
+    }
+
+
+
+
+    #endregion
+}
--- Real Time Strategy_ DarK TangO/Assets/Scripts/Units/Unit.cs
+++ Real Time Strategy_ DarK TangO/Assets/Scripts/Units/Unit.cs
@@ -3,20 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
 
 public class Unit : NetworkBehaviour
 {
-    [SerializeField] private UnityEvent  OnSelect=null;
-    [SerializeField] private UnityEvent  OnDeselect =null;
+    [SerializeField] private UnityEvent OnSelect = null;
+    [SerializeField] private UnityEvent OnDeselect = null;
 
     [SerializeField] private UnitMovement unitMovement = null;
-    
+
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    #region Server
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+    #endregion
+
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
-    #region Client
+   
     [Client]
     public void Select()
     {
